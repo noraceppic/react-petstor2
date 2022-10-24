a175ffc5e6e05d301c0e4cba698fef1ed3177c58
noraceppic <messanora86@gmail.com>
2022-10-24 15:30:36 +0200

    creation page contact

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,5 +1,90 @@
+import './contact.css'
+import './form.css'
+
 export default function Contact() {
     return(
-        <h1>Contact</h1>
-    )
+   <>
+        <div className="hearder">
+        <h2>Contacter Nous</h2>
+      </div>
+      
+      <div className="box">
+          <div className="left">
+              <div className="box-left">
+                  <form action="index.php?page=contact" method="post">
+                      <div className="form-group">
+                          <label for="Nom"> Nom :</label>
+                          <input type="text" name="Nom" required/>  
+                      </div>
+                      <div className="form-group">
+                          <label for="mail ">Email:</label>
+                          <input type="email" name="mail" required/>
+                      </div>
+                      <div className="form-group">
+                          <label for="Numéro de télephone">Numéro de téléphone: </label>
+                          <input type="text" name="Numéro de télephone" required/>
+                      </div>
+                      <div className="form-group">
+                          <label for="msg">Comment pouvons-nous vous aider ? : </label>
+                          <textarea id="msg" name="user_message"></textarea>
+      
+                      </div>
+                      <div className="toto">
+                          <input type="submit" value="Envoyer" id="submit"/>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      
+      
+          <div className="container-right">
+              <div class="box-right">
+                  <ul id="right-column">
+                      <li>
+                          <div className="icone"><i class="fa-solid fa-location-dot"></i> Address: 194 Rue de la Ronce, 76230 Isneauville </div>
+                      </li>
+                      <li>
+                          <div className="icone"><i class="fa-solid fa-phone"></i> Téléphone: 07 50 50 50 50 </div>
+                      </li>
+                      <li>
+                          <div className="icone"><i class="fa-solid fa-envelope-circle-check"></i> Email: contact@petstor.com</div>
+                      </li>
+      
+                  </ul>
+      
+                  <ul id="reseauxSociaux">
+      
+                      <a href="#">
+                          <li>
+                              <div className="iconeReseaux"><i class="fa-brands fa-linkedin  fa-2xl"></i> </div>
+      
+                          </li>
+                      </a>
+                      <a href="#">
+                          <li>
+                              <div className="iconeReseaux"><i class="fa-brands fa-facebook-square  fa-2xl"></i> </div>
+      
+                          </li>
+                      </a>
+                      <a href="#">
+                          <li>
+                              <div className="iconeReseaux"><i class="fa-brands fa-twitter fa-2xl"></i></div>
+      
+                          </li>
+                      </a>
+                      <a href="#">
+                          <li>
+                              <div className="iconeReseaux"><i class="fa-brands fa-instagram fa-2xl"></i> </div>
+      
+                          </li>
+                      </a>
+      
+                  </ul>
+      
+      
+              </div>
+          </div>
+      </div>
+   </> 
+   )
 };
